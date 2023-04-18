274fb1d1bb7250bf2028154223df422c0f47d1c1
Jon Magnuson <jon@bright.ai>
2023-04-18 09:26:16 -0500

    compiles, but i think the task should just own the ref to the NS*Stream

--- examples/connect.rs
+++ examples/connect.rs
@@ -1,10 +1,17 @@
+use std::collections::VecDeque;
 use std::error::Error;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 use std::time::Duration;
 
 use bluest::{btuuid, Adapter};
 use futures_util::StreamExt;
+use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::task::LocalSet;
 use tracing::{error, info};
 use tracing::metadata::LevelFilter;
+use bluest::corebluetooth::types::{NSInputStream, NSOutputStream};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -82,4 +89,67 @@ async fn main() -> Result<(), Box<dyn Error>> {
     info!("disconnected!");
 
     Ok(())
+}
+
+struct NSInputStreamWrapper<'objc, 'executor>{
+    inner: &'objc NSInputStream,
+    istream_rx: UnboundedReceiver<Vec<u8>>,
+    rx_buf: VecDeque<u8>,
+    local_executor: &'executor LocalSet,
+    // read_task:
+}
+
+impl<'a, 'b> AsyncRead for NSInputStreamWrapper<'a, 'b> {
+    fn poll_read(mut self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut ReadBuf<'_>) -> Poll<std::io::Result<()>> {
+        let slef = (&mut (*self.as_mut()));
+        match Pin::new(&mut slef.istream_rx).poll_recv(cx) {
+            Poll::Ready(bytes) => {
+                let mut newv: VecDeque<_> = bytes.expect("rx terminated while polling").into();
+                slef.rx_buf.append(&mut newv)
+            }
+            Poll::Pending => {}
+        }
+
+        let n_to_read = std::cmp::min(buf.remaining(), slef.rx_buf.len());
+        if n_to_read > 0 {
+            // TODO: more optimalizing
+            let v = slef.rx_buf.drain(..n_to_read).collect::<Vec<u8>>();
+            buf.put_slice(v.as_slice());
+
+            Poll::Ready(Ok(()))
+        } else {
+            Poll::Pending
+        }
+
+        /*if !rx_buf.is_empty() {
+            // n_to_read should only be zero if remaining is zero
+            let
+            let v = std::mem::take(slef.rx_buf);
+
+            return Poll::Ready(());
+        }*/
+    }
+}
+
+struct NSOutputStreamWrapper<'objc>{
+    inner: &'objc NSOutputStream
+}
+
+impl<'a> AsyncWrite for NSOutputStreamWrapper<'a> {
+    fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<Result<usize, std::io::Error>> {
+        // if ostream.has_space_available() {
+            let nwrite = self.inner.write(buf, buf.len());
+        // }
+        Poll::Ready(Ok(nwrite as usize))
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
+        self.inner.close();
+
+        Poll::Ready(Ok(()))
+    }
 }
